8f3ca2c666320353089e0c15c3ff0ef84dbe35a3
Akhil <anadigat@andrew.cmu.edu>
2020-05-27 15:03:45 +0300

    Corrected syntax errors

--- block.go
+++ block.go
@@ -9,26 +9,26 @@ import (
 
 // Block structure
 type block struct {
-	timeStamp int64   // Time of block creation
-	data      []byte  // Information to be contained by block
-	prevHash  []byte  // Hash of previous block
-	currHash  []byte  // Hash of defined block
+	timeStamp int64  // Time of block creation
+	data      []byte // Information to be contained by block
+	prevHash  []byte // Hash of previous block
+	currHash  []byte // Hash of defined block
 }
 
 // Set hash value for a block
 func (block *block) setHash() {
-	timeStamp := []byte(strconv.FormatInt(b.timeStamp, 10))
-	headers := bytes.Join([][]byte{timeStamp, b.data, b.prevHash}, []byte{})
+	timeStamp := []byte(strconv.FormatInt(block.timeStamp, 10))
+	headers := bytes.Join([][]byte{timeStamp, block.data, block.prevHash}, []byte{})
 	hash := sha256.Sum256(headers)
 
-	b.Hash = hash[:]
-} 
+	block.currHash = hash[:]
+}
 
 // Create a new block
 func newBlock(data string, prevHash []byte) *block {
 	block := &block{time.Now().Unix(), []byte(data), prevHash, []byte{}}
 	block.setHash()
-	return block 
+	return block
 }
 
 // Create first block in a chain

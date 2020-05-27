72b30762a1eac9d156fd4dde47f31cdcd8121876
Akhil <anadigat@andrew.cmu.edu>
2020-05-27 15:11:05 +0300

    Fixed block and chain structures

--- chain.go
+++ chain.go
@@ -8,11 +8,11 @@ type chain struct {
 // Add a new block to the end of the chain
 func (chain *chain) addBlock(data string) {
 	lastBlock := chain.blocks[len(chain.blocks) - 1]
-	newBlock := newBlock(data, lastBlock.hash)
+	newBlock := newBlock(data, lastBlock.currHash)
 	chain.blocks = append(chain.blocks, newBlock)
 }
 
 // Create a new blockchain
-fun newChain() *chain {
+func newChain() *chain {
 	return &chain{[]*block{newFirstBlock()}}
 }
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	chain.addBlock("Test 1")
 	chain.addBlock("Test 2")
 
-	for _, block := range chain.block {
+	for _, block := range chain.blocks {
 		fmt.Printf("Previous Hash: %x\n", block.prevHash)
 		fmt.Printf("Current Hash: %x\n", block.currHash)
 		fmt.Printf("Data: %s\n", block.data)
